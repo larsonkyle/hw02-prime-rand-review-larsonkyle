e62eedad871cb054f3b2f442dddb08bba0dfbb42
larsonkyle <klarson22@saddleback.edu>
2023-02-10 17:32:59 -0800

    created gen_number functions and headers

--- src/gen_number.cpp
+++ src/gen_number.cpp
@@ -1,1 +1,64 @@
-// Please place all function definitions for gen_number.h here
+#include "gen_number.h"
+
+int generateNum() {
+	std::srand((unsigned) std::time(NULL));
+	return std::rand() % 100;
+}
+
+int doubleNum(int num) {
+	return num * 2;
+}
+
+int reverseDigit(int num) {
+	int reverseDigit = 0;
+	int lastDigit = 0;
+
+	while (num != 0) {
+		lastDigit = num % 10;
+		reverseDigit = reverseDigit * 10 + lastDigit;
+		num = num / 10;
+	}
+
+	return reverseDigit;
+}
+
+int sumDigits(int num) {
+	int num2 = num;
+	int sumDigit = 0;
+	int lastDigit;
+
+	while(num2 != 0) {
+		lastDigit = num2 % 10;
+		sumDigit += lastDigit;
+		num2 /= 10;
+	}
+	return sumDigit;
+}
+
+int raisePow2_3_4(int num, int exp) {
+	return pow(num, exp);
+}
+
+int raiseFirstDigitPowSecondDigit (int num) {
+	int secondDigit;
+
+	if (num > 9 && num < 99) {
+		secondDigit = num % 10;
+		num /= 10;
+		num = pow(num, secondDigit);
+	}
+	return num;
+}
+
+int raiseFirstAndSecondDigitPowThirdDigit(int num) {
+	int lastDigit;
+	
+	if (num > 99 && num < 999) {
+		lastDigit = num % 10;
+		if (lastDigit <= 4) {
+			num /= 10;
+			num = pow(num, lastDigit);
+		}
+	}
+	return num;
+}
--- src/gen_number.h
+++ src/gen_number.h
@@ -1,1 +1,21 @@
-// Place function prototypes for gen_number.cpp
+# ifndef _GEN_NUMBER_H_
+# define _GEN_NUMBER_H_
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+int generateNum();
+
+int doubleNum(int num);
+
+int reverseDigit(int num);
+
+int sumDigits(int num);
+
+int raisePow2_3_4(int num, int exp);
+
+int raiseFirstDigitPowSecondDigit(int num);
+
+int raiseFirstAndSecondDigitPowThirdDigit(int num);
+
+# endif
